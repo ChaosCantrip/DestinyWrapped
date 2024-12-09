79143097a0f85cbf33d11c13e59b15dffa338dfd
James Donald <dev@chaoscantrip.com>
2024-12-09 07:28:04 +0000

    [Cloud Functions] Added deploy.bat

---
+++ cloud_functions/deploy.bat
@@ -0,0 +1,22 @@
+@echo off
+
+for %%A in (GOOGLE_CLOUD_FUNCTION GOOGLE_CLOUD_DB GOOGLE_CLOUD_DOCUMENT GOOGLE_CLOUD_PROJECT GOOGLE_CLOUD_REGION GOOGLE_CLOUD_TRIGGER_LOCATION BUNGIE_API_KEY) do (
+    if not defined %%A (
+        echo %%A Environment Variable not set
+        exit /b 1
+    )
+)
+
+@echo on
+
+gcloud functions deploy %GOOGLE_CLOUD_FUNCTION% ^
+--gen2 ^
+--runtime=python312 ^
+--region=%GOOGLE_CLOUD_REGION% ^
+--trigger-location=%GOOGLE_CLOUD_TRIGGER_LOCATION% ^
+--source=. ^
+--entry-point=process_data ^
+--trigger-event-filters=type=google.cloud.firestore.document.v1.written ^
+--trigger-event-filters=database=%GOOGLE_CLOUD_PROJECT% ^
+--trigger-event-filters-path-pattern=document=%GOOGLE_CLOUD_DOCUMENT% ^
+--set-env-vars GOOGLE_CLOUD_PROJECT=%GOOGLE_CLOUD_PROJECT%,BUNGIE_API_KEY=%BUNGIE_API_KEY%
